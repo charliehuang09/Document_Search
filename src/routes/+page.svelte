b1dfc9c396ce2360e99b6f1a23525e6fd48e36d9
charliehuang09 <charliehuang09@gmail.com>
2024-10-12 14:29:00 -0700

    Init
    Signed-off-by: charliehuang09 <charliehuang09@gmail.com>

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+
+    async function login() {
+        goto("/teacher")
+	}
+</script>
+
+<button style="font-size: 1.2em; padding: 10px 20px;" on:click={login}>
+    Teacher Login
+</button>
