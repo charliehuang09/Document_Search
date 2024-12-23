2b5aaf3a673b3f6e0349cca91233c19718e97ee3
charliehuang09 <charliehuang09@gmail.com>
2024-12-23 14:37:43 -0800

    start text search feature
    Signed-off-by: charliehuang09 <charliehuang09@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,23 @@
 	async function login() {
 		goto('/teacher');
 	}
+
+	export let placeholder = 'Search...';
+	export let query = '';
+  async function search(){
+    console.log(query);
+		const response = await fetch('api/search', {
+			method: 'POST',
+			body: JSON.stringify({
+				query: "query"
+			}),
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+  };
 </script>
 
+<input type="text" {placeholder} bind:value={query} on:input={() => search()} />
 <button style="font-size: 1.2em; padding: 10px 20px;" on:click={login}> Teacher Login </button>
---
+++ src/routes/api/search/+server.ts
@@ -0,0 +1,23 @@
+// import { ChatbotCompletion } from '$lib';
+
+export async function POST(params: { request: Request }) {
+	const { query } = await params.request.json();
+
+  console.log("BOBOBOIadfg;oidjfsgioj");
+	// const completion_manager = new ChatbotCompletion(import.meta.env.VITE_OPENAI_API_KEY!, {
+	// 	openai_model: import.meta.env.VITE_OPENAI_MODEL_NAME!,
+	// 	verbose: true,
+	// 	do_history: true,
+	// 	generate_data: false,
+	// 	use_summarybot: true
+	// });
+	// await completion_manager.setup();
+	//
+	// const output = await completion_manager.query(chatHistory, input);
+
+	return new Response(JSON.stringify(query), {
+		headers: {
+			'Content-Type': 'text/plain'
+		}
+	});
+}
